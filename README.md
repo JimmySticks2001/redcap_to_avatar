417f163b49be6a92519cc13cad37cd427646ac94
Tim Lightfoot <tlightfo@gmail.com>
2016-06-14 10:05:35 -0500

    Finished big buttons.
    Removed old code left over from CANsee. Added size restrictions to
    webkit package.json.

--- README.md
+++ README.md
@@ -1,53 +1,12 @@
-# CAN see
+# REDcap to Avatar
 
-##### CAN bus sniffer to be used with Seeed Studio CAN-BUS Shield.
+##### Application to convert REDcap exported CSV files into an Avatar data import acceptable XML format.
 
-Inspired by [Martin Viljoen's project](http://jeepjkcanbus.blogspot.com/2014/04/martins-can-bus-sniffer-tool-v10.html), CANsee is an updated tool which is able to display the CAN messages sent across your vehicles internal network. It is built using [NW.js](http://nwjs.io/) (formerly Node Webkit) in the hopes that it will be Mac, Linux, and Windows frienly all from one code-base.
 
-![alt text](https://raw.githubusercontent.com/JimmySticks2001/CANsee/master/img/1.PNG)
 
 ##Things to do:
 
-* Send messages.
-
+* Everything.
 
 
 ##Things finished:
-
-* When message with same id has been received, increment Message count column.
-
-* Program Arduino to send messages formatted in the spec that CAN-BUS-TOOL uses so I can test this without having to connect to my car.
-
-* Convert CAN id to hex because it looks nice.
-
-* When successfully connected to a comm port, hide the connect button and show the disconnect button. When disconnected, hide the disconnect button and show the connect button.
-
-* Move the errors for modals into the modals. ex: the more comm ports there are the taller the comm port connect modal will be. This will cover up the error if there are lots of comm ports regardless of how tall the app window is. It also just makes sense to have the error notification in the active space, not in the greyed out background.
-
-* Make some sort of serial reconnect upon no data. For some reason I need to connect twice sometimes to get any data through. Perhaps if no data after a few seconds, reconnect.
-
-* Show spinner between comm port modal OK and successful connect.
-
-* Safely disconnect from serial on app close.
-
-* Format message column content so table column widths don't change.
-
-* When message has changed, highlight changed byte in Change column.
-  Perhaps just embolden the changed byte in the message column.
-
-* Save the names of the messages in localStorage.
-
-* Fix disconnect on app close. win not defined.
-  Removed safe disconnect. serialPort was no longer defined after close event fired. Perhaps browser-serialport does it's own closing on close event.
-
-* Make radio buttons for comm port selection look nice.
-
-* Fix comm port duplication when new comm port is selectable after app has started.
-
-* Add fail to retry after 3 attempts.
-
-* Removed handlebars lib.
-
-* Check for errors in the incoming message stream. Don't display errored messages on the table.
-
-* Fix names, you cant erase a name since it is sending in a null value.
--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
   "use strict";
 
-  //browser-serialport node module
-  var SerialPort = require("browser-serialport");
-  var serialPort;
-  var comm_port_count = 0;
-  var retry_attempts = 0;
-
   //filesystem node module
   var fs = require('fs');
 
@@ -14,125 +8,6 @@ $(document).ready(function() {
   var win = nw.Window.get();
   // Show the dev tools at app startup
   win.showDevTools();
-
-  //show the comm port connect button and hide the disconnect button.
-  $("#dialog_comm_port_select").show();
-  $("#comm_port_disconnect").hide();
-  //hide the loading spinner
-  $(".mdl-spinner").hide();
-
-
-  //
-  //Comm port selection
-  //
-  var comm_port_dialog = document.querySelector('#comm_port_select');
-  document.querySelector('#dialog_comm_port_select').addEventListener('click', function() {
-    //get the list of available comm ports if any
-    SerialPort.list(function(err, ports) {
-      if(ports.length > 0){
-        if(comm_port_count != ports.length){
-          //remove the old comm port radio buttons, the ports will change if something is plugged in or removed.
-          $(".comm_radios").remove();
-
-          //put a radio button for each available comm port
-          ports.forEach(function(port, index) {
-            console.log(port.comName);
-            var p = document.createElement("p");
-            p.className = "comm_radios";
-            var label = document.createElement("label");
-            label.className = "mdl-radio mdl-js-radio mdl-js-ripple-effect";
-            label.setAttribute("for", "option-" + index);
-
-            var input = document.createElement("input");
-            input.setAttribute("type", "radio");
-            input.setAttribute("id", "option-" + index);
-            input.setAttribute("name", "options");
-            input.setAttribute("value", port.comName);
-            input.className = "mdl-radio__button";
-
-            var span = document.createElement("span");
-            span.className = "mdl-radio__label";
-            span.appendChild(document.createTextNode(port.comName));
-
-            //componentHandler.upgradeElement(label);
-
-            $(label).append(input);
-            $(label).append(span);
-            $(p).append(label);
-            //$(input).after(document.createTextNode(port.comName));
-            $('#comm_port_select .mdl-dialog__content').prepend(p);
-
-            //"<p>" +
-            //  "<label class='mdl-radio mdl-js-radio mdl-js-ripple-effect' for='option-1'>" +
-            //    "<input type='radio' id='option-1' class='mdl-radio__button' name='options' value='1' checked>" +
-            //    "<span class='mdl-radio__label'>COM1</span>" +
-            //  "</label>" +
-            //"</p>"
-            componentHandler.upgradeDom();
-            comm_port_count = ports.length;
-          });
-        }//end if port count changed
-
-        //display the comm port select dialog
-        comm_port_dialog.showModal();
-      }else{
-        notification('alert', 'Unable to find any comm ports');
-      }
-    });
-  });
-
-  comm_port_dialog.querySelector('#comm_port_select_close').addEventListener('click', function() {
-    comm_port_dialog.close();
-  });
-
-  comm_port_dialog.querySelector('#comm_port_select_ok').addEventListener('click', function() {
-    serialConnect($("input[name='options']:checked").val(), $("#comm_baud").val());
-    comm_port_dialog.close();
-  });
-
-
-  //
-  //message detail
-  //
-  var message_detail_dialog = document.querySelector('#message_detail_dialog');
-  var id;
-  $('tbody').on('click', 'tr', function() {
-    //get the message id from the tr id attribute
-    id = this.id;
-    //get the name from the table
-    var name = $("#"+id+"_name").text();
-    //update the modal title with the can id
-    $("#modal_can_id").text(id);
-    //update the name text field to show the name from the table
-    $("#modal_name").val(name);
-    message_detail_dialog.showModal();
-  });
-
-  //message dialog clicked cancel
-  message_detail_dialog.querySelector('#message_select_close').addEventListener('click', function() {
-    //close the dialog
-    message_detail_dialog.close();
-    //clear the name text field
-    $("#modal_name").val("");
-  });
-
-  //message dialog clicked OK
-  message_detail_dialog.querySelector('#message_select_ok').addEventListener('click', function() {
-    var entered_name = $("#modal_name").val();
-
-    if(/[^\w ]+/.test(entered_name)){ //if contains special characters...
-      notification('alert', 'Name can not contain special characters');
-    }else if(/.{41,}/.test(entered_name)){
-      notification('alert', 'Name can not exceed 40 characters');
-    }else{
-      //put updated name in the table
-      changeRow(id, null, $("#modal_name").val());   //id, message, name
-      //close the dialog
-      message_detail_dialog.close();
-      //clear the name text field
-      $("#modal_name").val("");
-    }
-  });
 
 
   //
@@ -154,106 +29,18 @@ $(document).ready(function() {
 
 
   //
-  //Test error Notification
+  //Test notification
   //
-  //$("#error_test").click(function(){
-  //  notification('alert', 'This is an error');
-  //});
+  $("#button_redcap").click(function(){
+    notification("alert", "Something borked")
+  });
+  $("#button_avatar").click(function(){
+    notification("info", "Something happened")
+  });
+
+});
 
 
-  //
-  //Manage serial connection here.
-  //
-  var serial_retry;
-  function serialConnect(port, baudrate){
-    serialPort = new SerialPort.SerialPort(port, { baudrate: baudrate });
-    var message_buffer = '';
-    //this could be a reconnect attempt, so remove the old timer so a new one can be made.
-    clearInterval(serial_retry);
-
-    serialPort.on("open", function(){
-      console.log("connection open");
-      //hide the comm port connect button after successfull connect. Wait to show the disconnect button until after message received.
-      $("#dialog_comm_port_select").hide();
-      //show the spinner
-      $(".mdl-spinner").show();
-
-      //set a timer. If nothing is received after 3 seconds, reconnect.
-      serial_retry = setInterval(function(){
-        //if 3 attempts have been made but no connection, stop trying to connect.
-        if(retry_attempts >= 3){
-          retry_attempts = 0;
-          //cancel the retry attempts.
-          clearInterval(serial_retry);
-          //display an error.
-          notification('alert', 'Unable to connect to comm port');
-          //show the comm dialog with the error in it.
-          comm_port_dialog.showModal();
-          //hide the spinner and show the connect button.
-          $(".mdl-spinner").hide();
-          $("#dialog_comm_port_select").show();
-          //disconnect from the port and destroy the serialPort.
-          serialPort.close();
-          serialPort = null;
-        }else{
-          retry_attempts++;
-          console.log("retrying connection");
-          serialPort.close();
-          serialPort = null;
-          serialConnect($("input[name='options']:checked").val(), $("#comm_baud").val());
-        }
-      }, 3000);
-
-      serialPort.on("error", function(error){
-        notification('alert', error.message);
-      });
-
-      serialPort.on("data", function(data){
-        //if data is received, remove the timer.
-        clearInterval(serial_retry);
-        //hide the spinner and show the disconnect button.
-        $(".mdl-spinner").hide();
-        $("#comm_port_disconnect").show();
-        //push data into buffer so we can keep it around while playing with it.
-        message_buffer += data.toString();
-
-        //test against the regex pattern to make sure we have a complete messsage. If it doesn't match this
-        //format it is not a meesage we are looking for.
-        var match = message_buffer.match(/<\d{1,4},\d{1,3},\d{1,3},\d{1,3},\d{1,3},\d{1,3},\d{1,3},\d{1,3},\d{1,3}>/)[0];
-        if(match){
-          //take off the <> and split on ,
-          var temp_buff = match.toString().replace('<','').replace('>','').split(',');
-          //convert string id to string hex id
-          var temp_can_id = decimalStringToHexString(temp_buff[0]);
-          //remove the CAN id from the message
-          temp_buff.splice(0,1);
-          //update the row this message was for.
-          changeRow(temp_can_id, temp_buff);
-          //clear the message, start over.
-          message_buffer = '';
-        }
-      });
-    });
-
-    serialPort.on("error", function(error){
-      notification('alert', error.message);
-    });
-
-    //
-    //Disconnect from comm port when icon clicked
-    //
-    $("#comm_port_disconnect").click(function(){
-      serialPort.close();
-      console.log("connection closed");
-      //show the comm port connect button after disconnect and show the connect button.
-      $("#dialog_comm_port_select").show();
-      $("#comm_port_disconnect").hide();
-    });
-
-  };//end serialConnect
-
-
-});//end doc ready
 
 
 //
@@ -276,72 +63,6 @@ function notification(type, message){
 
   $(".notification").html("<p>" + message + "</p>").slideDown(250);
 };//end notification
-
-
-//
-//Add or edit a table row. Edits info on an existing row or adds it if it doesn't exist.
-//
-function changeRow(id, message, name) {
-  //see if any rows have the same can id
-  if($("tr[id*="+id+"]").length == 0){  //if it isn't found...
-    //get the name from localStorage
-    var saved_name = localStorage.getItem(id);
-
-    //create a new tr with the message info
-    $("tbody").append(
-      "<tr id="+id+">" +
-        "<td class='mdl-data-table__cell--non-numeric' id='"+id+"_id'>"+id+"</td>" +
-        "<td class='mdl-data-table__cell--non-numeric' id='"+id+"_message'>"+
-          "<div id='byte0' class='bytes'>"+message[0]+",</div>"+
-          "<div id='byte1' class='bytes'>"+message[1]+",</div>"+
-          "<div id='byte2' class='bytes'>"+message[2]+",</div>"+
-          "<div id='byte3' class='bytes'>"+message[3]+",</div>"+
-          "<div id='byte4' class='bytes'>"+message[4]+",</div>"+
-          "<div id='byte5' class='bytes'>"+message[5]+",</div>"+
-          "<div id='byte6' class='bytes'>"+message[6]+",</div>"+
-          "<div id='byte7' class='bytes'>"+message[7]+"</div>"+
-        "</td>" +
-        "<td class='mdl-data-table__cell--non-numeric' id='"+id+"_name'>"+(saved_name != null ? saved_name : '')+"</td>" +
-        "<td class='mdl-data-table__cell--non-numeric' id='"+id+"_count'>1</td>" +
-      "</tr>"
-    );
-  }else{  //if there is a row with this id already...
-    //if there is a message, update the table bytes with the new numbers.
-    if(message){
-      for(var i = 0; i <= 7; i++){
-        //first, get the old byte from the table.
-        var temp_byte = $("#"+id+" #"+id+"_message #byte"+i+"").text().replace(',','');
-        //compare the old and new value and bold if changed
-        if(temp_byte != message[i]){
-          $("#"+id+" #"+id+"_message #byte"+i+"").text(message[i]+(i == 7 ? "" : ",")).css("font-weight","Bold");
-          sessionStorage.setItem(id+"_"+i, 1);
-        }else{
-          //get byte change count from sessionStorage.
-          var byteChageCount = sessionStorage.getItem(id+"_"+i);
-          if(byteChageCount){ //if it exists...
-            if(byteChageCount >= 3){  //if it equals 3 remove the bold, else keep it.
-              $("#"+id+" #"+id+"_message #byte"+i+"").text(message[i]+(i == 7 ? "" : ",")).css("font-weight","Normal");
-            }else {
-              $("#"+id+" #"+id+"_message #byte"+i+"").text(message[i]+(i == 7 ? "" : ","));
-            }
-            sessionStorage.setItem(id+"_"+i, ++byteChageCount);
-          }else {
-            sessionStorage.setItem(id+"_"+i, 1);
-          }
-        }
-      }
-      //get the old count from the table, increment it, and stick it back into the table.
-      $("#"+id+"_count").text(parseInt($("#"+id+"_count").text()) + 1);
-    }
-    //update the name if given.
-    if(typeof name !== 'undefined'){
-      //save the new name in localStorage for future use.
-      localStorage.setItem(id, name);
-      $("#"+id+"_name").text(name);
-    }
-  }//end else
-
-};//end changeRow
 
 
 //
